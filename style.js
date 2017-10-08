f1ea534d19dea7895c2ac172f12107e00dd1c0f7
Janavi Anand <janavianand@yahoo.com>
2017-10-08 10:53:40 -0400

    first commit

---
+++ style.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function() {
+  $(".heading").css("background-color","#091022");
+  $(".heading").css("color","white");
+    $("nav").css("background-color","#e3f2fd");
+    $("nav").css("font-size",16);
+    $(".size").css("font-size",16);
+    $(".size14").css("font-size",14);
+    $(".white").css("color","gray");
+    $(".white").css("text-decoration","underline");
+    $(".well1").css("background-color","#1B2631");
+    $(".well1").css("color","white");
+});
+                    
